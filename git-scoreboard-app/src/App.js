7c751ad45c269e11411fc227c0d188ee7f199f23
Brian Ball <ideabrian@gmail.com>
2023-08-06 20:00:18 -0700

    Add react app

---
+++ git-scoreboard-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Scoreboard from './Scoreboard.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"><h1>Git Scoreboard</h1>
+      </header>
+        <main>
+          <Scoreboard />
+        </main>
+    </div>
+  );
+}
+
+export default App;
